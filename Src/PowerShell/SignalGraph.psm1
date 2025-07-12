625fe5a661cd88d15d134bf5219e105b14c5f69c
SiliconDreamArtists <development@sda.studio>
2025-07-12 16:30:20 -0700

    Current cf1c3b59-8e87-4f69-8ef6-3c90db07643e

--- Src/PowerShell/SignalGraph.psm1
+++ Src/PowerShell/SignalGraph.psm1
@@ -4,7 +4,6 @@
 . "$PSScriptRoot/Classes/Graph.ps1"
 
 . "$PSScriptRoot/Utilities/Get-ResolvedValueFromPathSignal.ps1"
-. "$PSScriptRoot/Utilities/Invoke-GSGProcessingRules.ps1"
 . "$PSScriptRoot/Utilities/Convert-JsonToSignalGraph.ps1"
 . "$PSScriptRoot/Utilities/Add-PathToDictionary.ps1"
 . "$PSScriptRoot/Utilities/Resolve-PathHydration.ps1"
@@ -12,22 +11,22 @@
 . "$PSScriptRoot/Utilities/Remove-PathFromDictionary.ps1"
 . "$PSScriptRoot/Utilities/Resolve-GraphPlanInjectionContext.ps1"
 . "$PSScriptRoot/Utilities/Move-PathInDictionary.ps1"
-. "$PSScriptRoot/Utilities/Invoke-FormulaGraphCondenser.ps1"
+#. "$PSScriptRoot/Utilities/Invoke-FormulaGraphCondenser.ps1"
 . "$PSScriptRoot/Utilities/Resolve-PathFormulaGraphForJsonArray.ps1"
-. "$PSScriptRoot/Utilities/Invoke-GraphPlanOnItem.ps1"
+#. "$PSScriptRoot/Utilities/Invoke-GraphCondenser.ps1"
 . "$PSScriptRoot/Utilities/Parse-FilterSegment.ps1"
+#. "$PSScriptRoot/Utilities/Invoke-PlanAndDependents.ps1"
 
 Export-ModuleMember -Function Get-ResolvedValueFromPathSignal
-Export-ModuleMember -Function Invoke-GSGProcessingRules
 Export-ModuleMember -Function Convert-JsonToSignalGraph
 Export-ModuleMember -Function Add-PathToDictionary
 Export-ModuleMember -Function Resolve-PathHydration
-Export-ModuleMember -Function Resolve-PathFromDictionary
 Export-ModuleMember -Function Resolve-PathFromDictionary
 Export-ModuleMember -Function Resolve-GraphPlanInjectionContext
 Export-ModuleMember -Function Move-PathInDictionary
 Export-ModuleMember -Function Invoke-FormulaGraphCondenser
 Export-ModuleMember -Function Resolve-PathFormulaGraphForJsonArray
 Export-ModuleMember -Function Parse-FilterSegment
-Export-ModuleMember -Function Invoke-GraphPlanOnItem
+#Export-ModuleMember -Function Invoke-GraphCondenser
+#Export-ModuleMember -Function Invoke-PlanAndDependents
 
